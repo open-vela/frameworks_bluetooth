e03bb469d228a0669b4f4b539c6868db1187bde2
yuxingyang <yuxingyang@xiaomi.com>
2021-12-08 22:07:50 +0800

    add rssi value
    Signed-off-by: yuxingyang <yuxingyang@xiaomi.com>

--- btservice/gap/bts_gap.c
+++ btservice/gap/bts_gap.c
@@ -390,6 +390,7 @@ static void adapter_device_found_callback(remote_device_t* device)
     new_device->addr_type = device->addr_type;
     new_device->device_type = device->device_type;
     new_device->cod = device->cod;
+    new_device->rssi = device->rssi;
     gap_msg_t* msg = gap_msg_new(GAP_DEVICE_FOUND);
     msg->event_data.data.device = new_device;
     gap_send_message(msg);
