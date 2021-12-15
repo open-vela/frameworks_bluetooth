d15a0b0c89760f610980602fca7460b6a5310953
chengkai <chengkai@xiaomi.com>
2021-12-15 14:55:54 +0800

    Add detail info for gattc tool
    VELAPLATFO-170
    
    Signed-off-by: chengkai <chengkai@xiaomi.com>
    Change-Id: I4c9ebb4465830f92dd6e2bb9c1ea4c7aba8a1784

--- utils/utils.c
+++ utils/utils.c
@@ -30,10 +30,10 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #include "btm_manager.h"
 #include "utils.h"
