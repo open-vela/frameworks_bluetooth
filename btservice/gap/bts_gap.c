66517dc39a17f714b8e44f6844d2656435e55cfe
fangzhenwei <fangzhenwei@xiaomi.com>
2022-01-12 16:32:59 +0800

    bluetooth/gap:show bonded device linkkey
    VELAPLATFO-1288
    
    Change-Id: I652c2a07cb5c6cff5281f3255c66f03eeeee310b
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- btservice/gap/bts_gap.c
+++ btservice/gap/bts_gap.c
@@ -944,8 +944,12 @@ int bts_get_bonded_devices(bt_device_t* device_list, int max_out)
     SERVICE_REMOTE_DEVICE_S* bonded_list = malloc(sizeof(SERVICE_REMOTE_DEVICE_S) * max_out);
     ret = service_adapter_gap_get_bonded_devices(bonded_list, max_out);
     for (int i = 0; i < ret; i++) {
+        uint8_t *link_key = bonded_list[i].link_key;
         memcpy(device_list[i].addr, bonded_list[i].bd_addr, BT_ADDR_LENGTH);
         memcpy(device_list[i].name, bonded_list[i].bt_name, DEVICE_NAME_MAX_LEN + 1);
+        BT_LOGD("Linkkey: %02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X", link_key[0], link_key[1], link_key[2], link_key[3], link_key[4], link_key[5],
+                       link_key[6], link_key[7], link_key[8], link_key[9], link_key[10], link_key[11],
+                       link_key[12], link_key[13], link_key[14], link_key[15]);
     }
     if (bonded_list)
         free(bonded_list);
