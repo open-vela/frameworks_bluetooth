bac6fa66fd4777a8b63cfc2704188df98c262c75
fangzhenwei <fangzhenwei@xiaomi.com>
2022-10-17 22:10:35 +0800

    bluetooth:a2dp mute don't check stream state
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- btservice/a2dp/bts_a2dp_control.c
+++ btservice/a2dp/bts_a2dp_control.c
@@ -178,7 +178,7 @@ static void bts_a2dp_control_on_stop(uint8_t ch_id)
     }
 #endif
 #ifdef CONFIG_BLUETOOTH_A2DP_SINK
-    if (ch_id == A2DP_IPC_CH_ID_AV_SINK_CTRL && bts_a2dp_sink_stream_started()) {
+    if (ch_id == A2DP_IPC_CH_ID_AV_SINK_CTRL) {
         bts_a2dp_sink_mute();
     }
 #endif
