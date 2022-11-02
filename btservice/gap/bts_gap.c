aa606f98bcd79d0453bfd67c0b7b4b58a0978050
fangzhenwei <fangzhenwei@xiaomi.com>
2022-11-02 17:54:34 +0800

    bluetooth:framework upgrade plan named Obelisk
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- btservice/gap/bts_gap.c
+++ btservice/gap/bts_gap.c
@@ -34,9 +34,7 @@
 #include "bts_service_interface.h"
 #include "stack_adapter_common.h"
 #include "stack_adapter_gap.h"
-#ifdef CONFIG_BLUETOOTH_VENDOR_DEPENDENCY
-#include "controller.h"
-#endif
+
 #include "utils.h"
 
 #define LOG_TAG "bts_gap"
@@ -173,10 +171,7 @@ typedef struct {
 
 struct list_node* g_msg_list;
 static bts_gap_callback_t* g_bts_gap_callbacks = NULL;
-#ifdef CONFIG_BLUETOOTH_VENDOR_DEPENDENCY
-static bool g_keep_alive_enable = false;
-static uv_timer_t *g_alive_timer = NULL;
-#endif
+
 /*process callback from stack */
 
 extern void InitTransportLayer(void);
@@ -201,25 +196,6 @@ static void gap_msg_destory(gap_msg_t* msg)
     free(msg);
 }
 
-#ifdef CONFIG_BLUETOOTH_VENDOR_DEPENDENCY
-static void do_check_controller_alive(char *data)
-{
-    if (!controller_check_alive()) {
-        controller_dump_state();
-        ASSERT(0);
-    }
-}
-
-static void bts_gap_controller_keep_alive(bool enable)
-{
-    BT_LOGD("%s, state:%d, en:%d", __func__, g_keep_alive_enable, enable);
-    if (enable && !g_keep_alive_enable)
-        g_alive_timer = start_timer(KEEP_ALIVE_INTERVAL, KEEP_ALIVE_INTERVAL, do_check_controller_alive, NULL);
-    else if (!enable && g_keep_alive_enable)
-        stop_timer(g_alive_timer);
-}
-#endif
-
 static void process_loop_in_gap(void* data, size_t data_size)
 {
 
@@ -232,12 +208,6 @@ static void process_loop_in_gap(void* data, size_t data_size)
     case GAP_STACK_STATE_CHANGED: {
         bt_service_state state = gap_msg->event_data.data.stack_state;
         const bluetooth_service_interface* service_interface = get_bluetooth_service_interface();
-#ifdef CONFIG_BLUETOOTH_VENDOR_DEPENDENCY
-        if (state == BTM_STATE_ON)
-            bts_gap_controller_keep_alive(true);
-        else if (state == BTM_STATE_OFF)
-            bts_gap_controller_keep_alive(false);
-#endif
         if (service_interface) {
             service_interface->stack_state_change(state);
         }
