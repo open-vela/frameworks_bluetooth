9b2166728bf247d5e6db18cf0085b22a18a46274
chengkai <chengkai@xiaomi.com>
2023-02-01 15:04:35 +0800

    btservice/hogp: add hogp  config and test tool
    neededby:904155
    
    Signed-off-by: chengkai <chengkai@xiaomi.com>

--- tools/hid_device.c
+++ tools/hid_device.c
@@ -131,50 +131,38 @@ const uint8_t s_hidMouseReportDesc[] = {
 };
 
 const static uint8_t s_hidComboReportDesc[] = {
+    0x05, 0x0C, // Usage Page (Consumer)
+    0x09, 0x01, // Usage (Consumer Control)
+    0xA1, 0x01, // Collection (Application)
+    0x85, 0x01, // Report Id (1)
+    0x15, 0x00, // Logical minimum (0)
+    0x25, 0x01, // Logical maximum (1)
+    0x75, 0x01, // Report Size (1)
+    0x95, 0x01, // Report Count (1)
+    0x09, 0xCD, // Usage (Play/Pause)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x0A, 0x83, 0x01, // Usage (AL Consumer Control Configuration)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x09, 0xB5, // Usage (Scan Next Track)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x09, 0xB6, // Usage (Scan Previous Track)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x09, 0xEA, // Usage (Volume Down)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x09, 0xE9, // Usage (Volume Up)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x0A, 0x25, 0x02, // Usage (AC Forward)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0x0A, 0x24, 0x02, // Usage (AC Back)
+    0x81, 0x06, // Input (Data,Value,Relative,Bit Field)
+    0xC0, // End Collection
+
     0x05, 0x01, /* Usage Page (Generic Desktop), */
     0x09, 0x02, /* Usage (Mouse), */
     0xA1, 0x01, /* Collection (Application), */
-    0x09, 0x06, /* Usage (Keyboard), */
-    0x85, 0x01, /* Report_ID (1), */
-    0xA1, 0x00, /* Collection (Physical), */
-    0x05, 0x07, /* Usage Page (Key Codes); */
-    0x19, 0xE0, /* Usage Minimum (224), */
-    0x29, 0xE7, /* Usage Maximum (231), */
-    0x15, 0x00, /* Logical Minimum (0), */
-    0x25, 0x01, /* Logical Maximum (1), */
-    0x75, 0x01, /* Report Size (1), */
-    0x95, 0x08, /* Report Count (8), */
-    0x81, 0x02, /* Input (Data, Variable, Absolute), ;Modifier byte */
-
-    0x95, 0x01, /* Report Count (1), */
-    0x75, 0x08, /* Report Size (8), */
-    0x81, 0x01, /* Input (Constant), ;Reserved byte */
-
-    0x95, 0x05, /* Report Count (5), */
-    0x75, 0x01, /* Report Size (1), */
-    0x05, 0x08, /* Usage Page (Page# for LEDs), */
-    0x19, 0x01, /* Usage Minimum (1), */
-    0x29, 0x05, /* Usage Maximum (5), */
-    0x91, 0x02, /* Output (Data, Variable, Absolute), ;LED report */
-
-    0x95, 0x01, /* Report Count (1), */
-    0x75, 0x03, /* Report Size (3), */
-    0x91, 0x01, /* Output (Constant), ;LED report padding */
-
-    0x95, 0x06, /* Report Count (6), */
-    0x75, 0x08, /* Report Size (8), */
-    0x15, 0x00, /* Logical Minimum (0), */
-    0x25, 0x65, /* Logical Maximum(101), */
-    0x05, 0x07, /* Usage Page (Key Codes), */
-    0x19, 0x00, /* Usage Minimum (0), */
-    0x29, 0x65, /* Usage Maximum (101), */
-    0x81, 0x00, /* Input (Data, Array), ;Key arrays (6 bytes) */
-    0xC0, /* End Collection */
-
-    0x05, 0x01, /* Usage Page (Generic Desktop), */
     0x09, 0x01, /* Usage (Pointer), */
-    0x85, 0x02, /* Report_ID (2), */
     0xA1, 0x00, /* Collection (Physical), */
+    0x85, 0x02, /* Report_ID (2), */
     0x05, 0x09, /* Usage Page (Buttons), */
     0x19, 0x01, /* Usage Minimum (01), */
     0x29, 0x03, /* Usage Maximun (03), */
@@ -196,26 +184,23 @@ const static uint8_t s_hidComboReportDesc[] = {
     0x25, 0x7F, /* Logical Maximum (127), */
     0x75, 0x08, /* Report Size (8), */
     0x95, 0x03, /* Report Count (3), */
-    0x81, 0x06, /* Input (Data, Variable, Relative),    ;3 position bytes (X & Y & Wheel) */
+    0x81, 0x06, /* Input (Data, Variable, Relative),    ;3 position bytes (X & Y
+                   & Wheel) */
     0xC0, /* End Collection, */
 
+    0x85, 0x03, /* REPORT ID(3) Function Key */
     0x05, 0x0C, /* Usage Page (Consumer), */
-    0x09, 0x01, /* Usage (Consumer Control), */
-    0x85, 0x03, /* Report_ID (3), */
-    0xA1, 0x00, /* Collection (Physical), */
-    0x0A, 0x23, 0x02, /* Usage (AC Home), */
+    0x09, 0xE9, /* Usage (Volume Increment) */
+    0x09, 0xEA, /* Usage (Volume Decrement) */
     0x0A, 0x24, 0x02, /* Usage (AC Back), */
-    0x09, 0x40,
-    0x09, 0x65, /* Usage (Snapshot) */
+    0x0A, 0x23, 0x02, /* Usage (AC Home), */
     0x95, 0x04, /* Report Count (4), */
     0x75, 0x01, /* Report Size (1), */
-    0x81, 0x02, /* Input (Data, Variable, Absolute),    ;3 button bits */
+    0x81, 0x02, /* Input (Data, Variable, Absolute), 4 button bits */
 
-    0x95, 0x01, /* Report Count (1), */
-    0x75, 0x04, /* Report Size (4), */
-    0x81, 0x01, /* Input (Constant),    ;6 bit padding */
-    0xC0, /* End Collection, */
-
+    0x95, 0x01, /* REPORT_COUNT (1) */
+    0x75, 0x04, /* REPORT_SIZE (4) */
+    0x81, 0x01, /* INPUT (Cnst) - Reserved Byte (I)*/
     0xC0 /* End Collection */
 };
 
