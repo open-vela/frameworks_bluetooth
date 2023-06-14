f18d2a485ccb4b0c38293e128e6d584dd8155de8
chao an <anchao@xiaomi.com>
2023-06-14 18:18:04 +0800

    bluetooth framework re-implement a2dp(sink/source) avrcp(target/control)
    VELAPLATFO-4605
    
    Change-Id: If29845d1953ac2139fa9b282980890770c3791d2
    Signed-off-by: chao an <anchao@xiaomi.com>
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- service/profiles/a2dp/a2dp_source_audio.h
+++ service/profiles/a2dp/a2dp_source_audio.h
@@ -1,6 +1,7 @@
 /****************************************************************************
  *
  *   Copyright (C) 2023 Xiaomi InC. All rights reserved.
+ *   Copyright (C) 2023 Xiaomi InC. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,6 +33,8 @@
  ****************************************************************************/
 #ifndef __A2DP_SOURCE_AUDIO_H__
 #define __A2DP_SOURCE_AUDIO_H__
+#ifndef __A2DP_SOURCE_AUDIO_H__
+#define __A2DP_SOURCE_AUDIO_H__
 
 #include "a2dp_codec.h"
 #include "bluetooth_define.h"
@@ -43,14 +46,19 @@
 
 typedef void (*frame_send_callback)(uint8_t *buf, uint16_t nbytes, uint8_t nb_frames, uint64_t timestamp);
 typedef int (*frame_read_callback)(uint8_t *buf, uint16_t frame_len);
+typedef int (*frame_read_callback)(uint8_t *buf, uint16_t frame_len);
 
 typedef struct {
+    void (*init)(void *param, uint32_t mtu,
+                 frame_send_callback send_cb,
+                 frame_read_callback read_cb);
     void (*init)(void *param, uint32_t mtu,
                  frame_send_callback send_cb,
                  frame_read_callback read_cb);
     void (*reset)(void);
     void (*cleanup)(void);
     void (*send_frames)(uint16_t header_reserve, uint64_t timestamp);
+    int (*get_interval_ms)(void);
     int (*get_interval_ms)(void);
 } a2dp_source_stream_interface_t;
 
