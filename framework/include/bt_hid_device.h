cb9119041f95a671c55ccbd15231901ae8795c9a
wangzhi7 <wangzhi7@xiaomi.com>
2023-12-05 13:00:35 +0800

    add the IPC interface of HID
    VELAPLATFO-22184
    
    Change-Id: I1f5b60ee891ed549d1bac94f66e0ccdb658525ca
    Signed-off-by: wangzhi7 <wangzhi7@xiaomi.com>

--- framework/include/bt_hid_device.h
+++ framework/include/bt_hid_device.h
@@ -34,12 +34,6 @@ extern "C" {
 #define HID_SDP_DESCRIPTOR_REPORT   (0x22)
 #define HID_SDP_DESCRIPTOR_PHYSICAL (0x23)
 
-/* Descriptor length of HID DEVICE INFO */
-#define MAX_HID_DEVICE_NAME 32
-#define MAX_HID_DEVICE_DES 100
-#define MAX_HID_DEVICE_PRI 32
-#define MAX_DSC_LIST_LENGTH 512
-
 /* * HID supported features - bit mask */
 #define HID_ATTR_MASK_VIRTUAL_CABLE        0x0001
 #define HID_ATTR_MASK_RECONNECT_INITIATE   0x0002
@@ -67,7 +61,7 @@ typedef struct {
     uint16_t ssr_max_latency;
     uint16_t ssr_min_timeout;
     uint16_t dsc_list_length; /* Length of desc_list */
-    uint8_t dsc_list[ MAX_DSC_LIST_LENGTH ]; /* List of descriptors. Each descriptor is constructed as: Type(1 Byte), Length(2 Bytes, Little Endian), Values(Length Bytes) */
+    uint8_t *dsc_list; /* List of descriptors. Each descriptor is constructed as: Type(1 Byte), Length(2 Bytes, Little Endian), Values(Length Bytes) */
 } hid_info_t;
 
 /**
@@ -75,9 +69,9 @@ typedef struct {
  *
  */
 typedef struct {
-    char name[MAX_HID_DEVICE_NAME];
-    char description[MAX_HID_DEVICE_DES];
-    char provider[MAX_HID_DEVICE_PRI];
+    const char *name;
+    const char *description;
+    const char *provider;
     hid_info_t hids_info;
 } hid_device_sdp_settings_t;
 
