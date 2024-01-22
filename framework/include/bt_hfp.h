fe97b98f87954754dcb8d19eb14dcc5df8e86671
gaozihao <gaozihao@xiaomi.com>
2024-01-22 15:48:28 +0800

    HFP: reduce the length of call list in socket message 16 to 8.
    Signed-off-by: gaozihao <gaozihao@xiaomi.com>

--- framework/include/bt_hfp.h
+++ framework/include/bt_hfp.h
@@ -32,7 +32,7 @@ extern "C" {
    for better compatibility.
    Also defined as BTA_HF_CLIENT_AT_MAX_LEN (512) at Android */
 #define HFP_AT_LEN_MAX 512
-#define HFP_CALL_LIST_MAX 16
+#define HFP_CALL_LIST_MAX 8
 
 
 typedef enum {
