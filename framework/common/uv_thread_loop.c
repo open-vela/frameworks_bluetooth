6a4f7897d34f94aff367084f2065dd373b1435b4
fangzhenwei <fangzhenwei@xiaomi.com>
2024-01-23 19:31:53 +0800

    fix coverity REVERSE_INULL warning
    VELAPLATFO-24951
    
    Change-Id: I65de6017234313a28007264e7cf60a00dd183989
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- framework/common/uv_thread_loop.c
+++ framework/common/uv_thread_loop.c
@@ -190,6 +190,10 @@ void thread_loop_exit(uv_loop_t *loop)
 {
     struct list_node *node;
     struct list_node *tmp;
+
+    if (!loop)
+        return;
+
     loop_priv_t *priv = loop->data;
 
     if (priv->is_running) {
