8d229b5fb69d02b57e359e8bbe8c4018afd3e0b0
shenyangsi <shenyangsi@xiaomi.com>
2024-03-30 16:38:01 +0800

    GATT: add BLUETOOTH_GATTS_MAX_ATTRIBUTE_NUM config
    VELAPLATFO-29842
    
    Change-Id: Icb137be8c66e8eb675bc943c28dd607b1fa41add
    Signed-off-by: shenyangsi <shenyangsi@xiaomi.com>

--- framework/socket/bt_gatts.c
+++ framework/socket/bt_gatts.c
@@ -152,20 +152,20 @@ bt_status_t bt_gatts_add_attr_table(gatts_handle_t srv_handle, gatt_srv_db_t *sr
     bt_message_packet_t packet;
     bt_status_t status;
     bt_gatts_remote_t *gatts_remote = (bt_gatts_remote_t *)srv_handle;
-    uint8_t *raw_data = packet.gatts_pl._bt_gatts_add_attr_table.data;
+    uint8_t *raw_data = (uint8_t *)packet.gatts_pl._bt_gatts_add_attr_table.attr_db;
     uint32_t data_length = sizeof(gatt_attr_db_t) * srv_db->attr_num;
     gatt_attr_db_t *attr_inst = srv_db->attr_db;
 
     CHECK_NULL_PTR(gatts_remote);
-    if (data_length > sizeof(packet.gatts_pl._bt_gatts_add_attr_table.data))
+    if (data_length > sizeof(packet.gatts_pl._bt_gatts_add_attr_table.attr_db))
         return BT_STATUS_PARM_INVALID;
 
-    memcpy(packet.gatts_pl._bt_gatts_add_attr_table.data, attr_inst, data_length);
+    memcpy(packet.gatts_pl._bt_gatts_add_attr_table.attr_db, attr_inst, data_length);
     raw_data += data_length;
     for (int i = 0; i < srv_db->attr_num; i++, attr_inst++) {
         if (attr_inst->rsp_type == ATTR_AUTO_RSP && attr_inst->attr_length) {
             data_length += attr_inst->attr_length;
-            if (data_length > sizeof(packet.gatts_pl._bt_gatts_add_attr_table.data))
+            if (data_length > sizeof(packet.gatts_pl._bt_gatts_add_attr_table.attr_db))
                 return BT_STATUS_PARM_INVALID;
 
             memcpy(raw_data, attr_inst->attr_value, attr_inst->attr_length);
