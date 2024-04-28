d671e31eb9000971ad8126b76ee361bccdc9117f
duqunbo <duqunbo@xiaomi.com>
2024-04-28 21:01:59 +0800

    Change the management of resources in the feature lifecycle so that our resources can be released in a timely manner
    Signed-off-by: duqunbo <duqunbo@xiaomi.com>

--- feature/src/system_bluetooth_bt_a2dpsink_impl.c
+++ feature/src/system_bluetooth_bt_a2dpsink_impl.c
@@ -26,37 +26,37 @@
 
 void system_bluetooth_bt_a2dpsink_onRegister(const char* feature_name)
 {
-    feature_bluetooth_init_bt_ins();
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
 void system_bluetooth_bt_a2dpsink_onCreate(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
 {
+    feature_bluetooth_init_bt_ins(FEATURE_BLUETOOTH_A2DPSINK);
     feature_bluetooth_set_bt_ins(handle);
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
 void system_bluetooth_bt_a2dpsink_onRequired(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
 {
-    feature_bluetooth_add_feature_callback(handle, A2DPSINK_FEATURE);
+    feature_bluetooth_add_feature_callback(handle, FEATURE_BLUETOOTH_A2DPSINK);
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
 void system_bluetooth_bt_a2dpsink_onDetached(FeatureRuntimeContext ctx, FeatureInstanceHandle handle)
 {
-    feature_bluetooth_free_feature_callback(handle, A2DPSINK_FEATURE);
+    feature_bluetooth_free_feature_callback(handle, FEATURE_BLUETOOTH_A2DPSINK);
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
 void system_bluetooth_bt_a2dpsink_onDestroy(FeatureRuntimeContext ctx, FeatureProtoHandle handle)
 {
     feature_bluetooth_clean_bt_ins(handle);
+    feature_bluetooth_uninit_bt_ins(FEATURE_BLUETOOTH_A2DPSINK);
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
 void system_bluetooth_bt_a2dpsink_onUnregister(const char* feature_name)
 {
-    feature_bluetooth_uninit_bt_ins();
     FEATURE_LOG_INFO("%s::%s()", file_tag, __FUNCTION__);
 }
 
