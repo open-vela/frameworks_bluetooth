a22cdd352e00a8d275746416c386db273b733317
gaozihao <gaozihao@xiaomi.com>
2024-04-29 18:52:15 +0800

    HFP: report call disconnected on dialing failed.
    Signed-off-by: gaozihao <gaozihao@xiaomi.com>

--- service/profiles/hfp_hf/hfp_hf_service.c
+++ service/profiles/hfp_hf/hfp_hf_service.c
@@ -955,6 +955,7 @@ void hfp_hf_on_at_command_result_response(bt_address_t* addr, uint32_t at_cmd_co
 {
     switch (at_cmd_code) {
     case HFP_ATCMD_CODE_ATD:
+    case HFP_ATCMD_CODE_BLDN:
         break;
     default:
         return;
--- service/profiles/include/hfp_define.h
+++ service/profiles/include/hfp_define.h
@@ -97,8 +97,9 @@ typedef struct {
 } hfp_codec_config_t;
 
 typedef enum {
-    HFP_ATCMD_CODE_ATA = 0x1,
-    HFP_ATCMD_CODE_ATD,
+    HFP_ATCMD_CODE_ATA = 0x01,
+    HFP_ATCMD_CODE_ATD = 0x02,
+    HFP_ATCMD_CODE_BLDN = 0x1A,
     HFP_ATCMD_CODE_UNKNOWN = 0xFFFF,
 } hfp_atcmd_code_t;
 
