62dd9d3cfdc2c83dc2092c2f87d75eaf2f605f77
Haishen Zhang <zhanghaishen@xiaomi.com>
2024-05-30 23:16:45 +0800

    Vela-Android: Add BTIF wrapper for ON/OFF/Inquiry/Pairing (2/5)
    VELAPLATFO-26429
    
    dependson:4078095 4115005
    
    1. Update Android.bp
    2. Redefine enum and macro that conflict with Android definition
    3. Fix a bug about socket to report all UUID array from Bluelet to APP clients
    
    Change-Id: I9ebf886442b4678b9b716846ef9225a3f615f72f
    Signed-off-by: Haishen Zhang <zhanghaishen@xiaomi.com>

--- framework/include/bt_status.h
+++ framework/include/bt_status.h
@@ -16,33 +16,53 @@
 
 #ifndef _BT_STATUS_H__
 #define _BT_STATUS_H__
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#if !defined(BTIF_WITH_VELA)
+/*
+ * hardware/libhardware/include/hardware/bluetooth.h
+ * This definition was copied from Android 13.
+ * DON NOT MODIFY IT !!!
+ */
 typedef enum {
-    BT_STATUS_SUCCESS = 0, /* Success */
+    BT_STATUS_SUCCESS, /* Success */
     BT_STATUS_FAIL, /* Failure */
-    BT_STATUS_NOT_ENABLED, /* Bluetooth service not enabled */
+    BT_STATUS_NOT_READY,
     BT_STATUS_NOMEM, /* Heap not enough */
     BT_STATUS_BUSY, /* Busy */
+    BT_STATUS_DONE, /* request already completed */
+    BT_STATUS_UNSUPPORTED,
     BT_STATUS_PARM_INVALID, /* Invalid parameter */
-    BT_STATUS_NOT_SUPPORTED, /* Profile or festure not enable, should set related config */
-    BT_STATUS_DEVICE_NOT_FOUND, /* Device not found */
-    BT_STATUS_SERVICE_NOT_FOUND, /* Profile service not enable */
-    BT_STATUS_NOT_FOUND, /* Can't get what you expect */
-    BT_STATUS_NO_RESOURCES, /* Can't alloc resource in manager */
-    BT_STATUS_ERROR_BUT_UNKNOWN, /* Unknown error */
-    BT_STATUS_IPC_ERROR, /* IPC communication error */
-
-    /* for acl connection status */
-    BT_STATUS_PAGE_TIMEOUT = 0x20,
+    BT_STATUS_UNHANDLED,
     BT_STATUS_AUTH_FAILURE, /* remote accepts AUTH request, but AUTH failure */
-    BT_STATUS_AUTH_REJECTED, /* remote rejects AUTH request */
     BT_STATUS_RMT_DEV_DOWN, /* remote device not in BT range */
-    BT_STATUS_RMT_DEV_TERMINATE, /* remote disconnect the link actively */
-    BT_STATUS_LOCAL_TERMINATED, /* local disconnect the link or cancel connecting */
+    BT_STATUS_AUTH_REJECTED, /* remote rejects AUTH request */
+    BT_STATUS_JNI_ENVIRONMENT_ERROR,
+    BT_STATUS_JNI_THREAD_ATTACH_ERROR,
+    BT_STATUS_WAKELOCK_ERROR
 } bt_status_t;
+#endif
+
+// Rename the status
+#define BT_STATUS_NOT_ENABLED BT_STATUS_NOT_READY /* Bluetooth service not enabled */
+#define BT_STATUS_NOT_SUPPORTED BT_STATUS_UNSUPPORTED /* Profile or festure not enable, should set related config */
+
+// !!! If Android add more definitioin of status, please don't forget to modify this definition!!!
+#define BT_STATUS_EXT_START (BT_STATUS_WAKELOCK_ERROR + 0x20)
+
+// Add new status
+#define BT_STATUS_ERROR_BUT_UNKNOWN (BT_STATUS_EXT_START + 0x01) /* Unknown error */
+#define BT_STATUS_NOT_FOUND (BT_STATUS_EXT_START + 0x02) /* Can't get what you expect */
+#define BT_STATUS_DEVICE_NOT_FOUND (BT_STATUS_EXT_START + 0x03) /* Device not found */
+#define BT_STATUS_SERVICE_NOT_FOUND (BT_STATUS_EXT_START + 0x04) /* Profile service not enable */
+#define BT_STATUS_NO_RESOURCES (BT_STATUS_EXT_START + 0x05) /* Can't alloc resource in manager */
+#define BT_STATUS_IPC_ERROR (BT_STATUS_EXT_START + 0x06) /* IPC communication error */
+#define BT_STATUS_PAGE_TIMEOUT (BT_STATUS_EXT_START + 0x07)
+#define BT_STATUS_RMT_DEV_TERMINATE (BT_STATUS_EXT_START + 0x08) /* remote disconnect the link actively */
+#define BT_STATUS_LOCAL_TERMINATED (BT_STATUS_EXT_START + 0x09) /* local disconnect the link or cancel connecting */
 
 #ifdef __cplusplus
 }
