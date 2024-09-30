a32dae2f3a65f50e042a11310617f5df86e9f400
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2024-09-30 19:02:46 +0800

    [bug fix] When bt off is executed, disconnect all acl connections.
    Originally, only the "adapter_disable" function was executed. An additional step was added before that, which disconnects all ACL connections. A timer was also started to wait for the "disconnect" event for 2 seconds, followed by cleaning up profile resources (by following the normal disable procedure).
    
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- service/src/adapter_internel.h
+++ service/src/adapter_internel.h
@@ -21,6 +21,7 @@
 #include "bt_adapter.h"
 #include "bt_device.h"
 #include "bt_status.h"
+#include "service_loop.h"
 #include <stdbool.h>
 
 enum {
@@ -187,8 +188,10 @@ enum {
 enum adapter_event {
     SYS_TURN_ON = 0,
     SYS_TURN_OFF,
+    SYS_TURN_OFF_SAFE,
     TURN_ON_BLE,
     TURN_OFF_BLE,
+    SYS_TURN_OFF_SAFE_TIMEOUT,
     /*
         Don't support BREDR-only mode. If the user chooses TURN_ON,
         we turn on ble first by default, and then turn on bt
@@ -201,6 +204,7 @@ enum adapter_event {
     BREDR_DISABLE_TIMEOUT,
     BREDR_ENABLE_PROFILE_TIMEOUT,
     BREDR_DISABLE_PROFILE_TIMEOUT,
+    BREDR_ACL_ALL_DISCONNECTED,
     BLE_ENABLED,
     BLE_DISABLED,
     BLE_PROFILE_ENABLED,
@@ -262,6 +266,7 @@ void adapter_init(void);
 void adapter_cleanup(void);
 bt_status_t adapter_enable(uint8_t opt);
 bt_status_t adapter_disable(uint8_t opt);
+bt_status_t adapter_disable_safe(uint8_t opt);
 bt_adapter_state_t adapter_get_state(void);
 bool adapter_is_le_enabled(void);
 bt_device_type_t adapter_get_type(void);
@@ -316,6 +321,7 @@ bond_state_t adapter_get_remote_bond_state(bt_address_t* addr, bt_transport_t tr
 bool adapter_is_remote_bonded(bt_address_t* addr, bt_transport_t transport);
 bt_status_t adapter_connect(bt_address_t* addr);
 bt_status_t adapter_disconnect(bt_address_t* addr);
+bt_status_t adapter_disconnect_safe(void);
 bt_status_t adapter_le_connect(bt_address_t* addr,
     ble_addr_type_t type,
     ble_connect_params_t* param);
