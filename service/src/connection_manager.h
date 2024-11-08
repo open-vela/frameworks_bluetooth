636900a3aea3f2246fe21dad228786219d933949
chejinxian1 <chejinxian1@xiaomi.com>
2024-11-08 22:20:20 +0800

    Bluetooth: Add the Connection Manager(CM) module to manage performance enhancement features on different ACLs.
    bug: v/43848
    
    Rootcause: Some ACL need to add some enhanced features to provide a better Bluetooth communication experience. This submission is a part of the project and is intended for building a management framework.
    
    Signed-off-by: chejinxian1 <chejinxian1@xiaomi.com>

--- service/src/connection_manager.h
+++ service/src/connection_manager.h
@@ -17,19 +17,7 @@
 #ifndef __BT_CONNECTION_MANAGER_H__
 #define __BT_CONNECTION_MANAGER_H__
 
-#include "bt_addr.h"
-#include "bt_status.h"
-
 void bt_cm_init(void);
 void bt_cm_cleanup(void);
-
-bt_status_t bt_cm_enable_enhanced_mode(bt_address_t* peer_addr, uint8_t mode);
-bt_status_t bt_cm_disable_enhanced_mode(bt_address_t* peer_addr, uint8_t mode);
-
-void bt_cm_process_disconnect_event(bt_address_t* addr, uint8_t transport, uint32_t hci_reason_code);
-void bt_cm_disconnected(bt_address_t* addr, uint8_t profile_id);
-void bt_cm_connected(bt_address_t* addr, uint8_t profile_id);
-bt_status_t bt_cm_device_connect(bt_address_t* addr, uint8_t transport);
-bt_status_t bt_cm_device_disconnect(bt_address_t* addr, uint8_t transport);
 
 #endif /*__BT_CONNECTION_MANAGER_H__*/
