530efc97af1c9823f51dcb48e0b5623fa91ab5fb
liuxiang18 <liuxiang18@xiaomi.com>
2024-12-04 13:54:03 +0800

    Android-Vela: Adapter SPP in Android 14
    O61-805
    
    open flag CONFIG_RPMSG_UART if defined(Android14)
    
    Change-Id: Iad5494022864fb7ff9258f63dc6be63cad0ec3b4
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- framework/include/bt_config.h
+++ framework/include/bt_config.h
@@ -97,13 +97,13 @@
 // Socket: via IPv4
 //#define CONFIG_NET_IPv4 1
 #define CONFIG_INADDR_LOOPBACK 0x0A000202
+// SPP
+#define CONFIG_RPMSG_UART 1
 
 /********************* O95 Project Only *********************/
 #if defined(ANDROID_12)
 // Socket: RPMsg
 #define CONFIG_BLUETOOTH_RPMSG_CPUNAME "ap"
-// SPP
-#define CONFIG_RPMSG_UART 1
 
 /********************* O61 Project Only *********************/
 #elif defined(ANDROID_14)
