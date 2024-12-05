b1f3a263160aa46b0223acfbcc22b031da897343
chengkai <chengkai@xiaomi.com>
2024-12-05 17:53:10 +0800

    bluetooth: add zblue le enable/disable
    VELAPLATFO-43329
    
    Change-Id: I875e1c10d6676a178b18833c48f8dbde668c5c24
    Signed-off-by: chengkai <chengkai@xiaomi.com>

--- service/stacks/include/sal_adapter_le_interface.h
+++ service/stacks/include/sal_adapter_le_interface.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- *  Copyright (C) 2022 Xiaomi Corporation
+ *  Copyright (C) 2024 Xiaomi Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,77 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
-#ifndef __SAL_ADAPTER_INTERFACE_H_
-#define __SAL_ADAPTER_INTERFACE_H_
+#ifndef __SAL_ADAPTER_LE_INTERFACE_H_
+#define __SAL_ADAPTER_LE_INTERFACE_H_
+
+#include <stdint.h>
 
 #include "bluetooth.h"
+#include "bluetooth_define.h"
 #include "bt_addr.h"
 #include "bt_status.h"
-#include <stdint.h>
-#ifdef CONFIG_BLUETOOTH_BLE_SUPPORT
-#include "bt_le_advertiser.h"
-#include "bt_le_scan.h"
-#include "scan_manager.h"
-#endif
-#include "bluetooth_define.h"
 #include "power_manager.h"
 #include "vhal/bt_vhal.h"
 
-#define BT_INVALID_CONNECTION_HANDLE 0xFFFF
-
-/* service adapter layer for LE */
-// #ifdef CONFIG_BLUETOOTH_BLE_SUPPORT
 bt_status_t bt_sal_le_init(const bt_vhal_interface* vhal);
 void bt_sal_le_cleanup(void);
 bt_status_t bt_sal_le_enable(bt_controller_id_t id);
 bt_status_t bt_sal_le_disable(bt_controller_id_t id);
 bt_status_t bt_sal_le_set_io_capability(bt_controller_id_t id, bt_io_capability_t cap);
-#ifdef CONFIG_BLUETOOTH_BLE_SCAN
-bt_status_t bt_sal_le_set_scan_parameters(ble_scan_params_t* params);
-/* maybe implement it in scan service */
-bt_status_t bt_sal_le_start_scan(void);
-bt_status_t bt_sal_le_stop_scan(void);
-#endif
-#ifdef CONFIG_BLUETOOTH_BLE_ADV
-bt_status_t bt_sal_le_start_adv(uint8_t adv_id,
-    ble_adv_params_t* params,
-    uint8_t* adv_data,
-    uint16_t adv_len,
-    uint8_t* scan_rsp_data,
-    uint16_t scan_rsp_len);
-bt_status_t bt_sal_le_stop_adv(uint8_t adv_id);
-#endif
-int bt_sal_get_le_connected_devices(bt_controller_id_t id);
-int bt_sal_get_le_whitelist_devices(bt_controller_id_t id);
-int bt_sal_get_le_resolvinglist_devices(bt_controller_id_t id);
 bt_status_t bt_sal_le_set_static_identity(bt_controller_id_t id, bt_address_t* addr);
 bt_status_t bt_sal_le_set_public_identity(bt_controller_id_t id, bt_address_t* addr);
-bt_status_t bt_sal_le_set_remote_irk(bt_controller_id_t id, bt_address_t* addr, ble_addr_type_t type, bt_128key_t irk);
-bt_status_t bt_sal_le_get_current_irk(bt_controller_id_t id);
 bt_status_t bt_sal_le_set_address(bt_controller_id_t id, bt_address_t* addr);
 bt_status_t bt_sal_le_get_address(bt_controller_id_t id);
 bt_status_t bt_sal_le_set_bonded_devices(bt_controller_id_t id, remote_device_le_properties_t* props, uint16_t prop_cnt);
-bt_status_t bt_sal_le_get_bonded_devices(bt_controller_id_t id, remote_device_le_properties_t* props, uint16_t* cnt);
-bt_status_t bt_sal_le_connect(bt_controller_id_t id, bt_address_t* addr,
-    ble_addr_type_t type,
-    ble_connect_params_t* params);
+bt_status_t bt_sal_le_get_bonded_devices(bt_controller_id_t id, remote_device_le_properties_t* props, uint16_t* prop_cnt);
+bt_status_t bt_sal_le_connect(bt_controller_id_t id, bt_address_t* addr, ble_addr_type_t type, ble_connect_params_t* params);
 bt_status_t bt_sal_le_disconnect(bt_controller_id_t id, bt_address_t* addr);
 bt_status_t bt_sal_le_create_bond(bt_controller_id_t id, bt_address_t* addr, ble_addr_type_t type);
 bt_status_t bt_sal_le_remove_bond(bt_controller_id_t id, bt_address_t* addr);
-bt_status_t bt_sal_le_smp_reply(bt_controller_id_t id, bt_address_t* addr,
-    bool accept,
-    bt_pair_type_t type,
-    uint32_t passkey);
+bt_status_t bt_sal_le_smp_reply(bt_controller_id_t id, bt_address_t* addr, bool accept, bt_pair_type_t type, uint32_t passkey);
 bt_status_t bt_sal_le_set_legacy_tk(bt_controller_id_t id, bt_address_t* addr, bt_128key_t tk_val);
 bt_status_t bt_sal_le_set_remote_oob_data(bt_controller_id_t id, bt_address_t* addr, bt_128key_t c_val, bt_128key_t r_val);
 bt_status_t bt_sal_le_get_local_oob_data(bt_controller_id_t id, bt_address_t* addr);
 bt_status_t bt_sal_le_add_white_list(bt_controller_id_t id, bt_address_t* addr, ble_addr_type_t addr_type);
 bt_status_t bt_sal_le_remove_white_list(bt_controller_id_t id, bt_address_t* addr, ble_addr_type_t addr_type);
-bt_status_t bt_sal_le_add_resolving_list(bt_controller_id_t id, bt_address_t* addr);
-bt_status_t bt_sal_le_remove_resolving_list(bt_controller_id_t id, bt_address_t* addr);
 bt_status_t bt_sal_le_set_phy(bt_controller_id_t id, bt_address_t* addr, ble_phy_type_t tx_phy, ble_phy_type_t rx_phy);
 bt_status_t bt_sal_le_set_appearance(bt_controller_id_t id, uint16_t appearance);
 uint16_t bt_sal_le_get_appearance(bt_controller_id_t id);
-bt_status_t bt_sal_le_enable_key_derivation(bt_controller_id_t id, bool brkey_to_lekey,
-    bool lekey_to_brkey);
-#endif /* __SAL_ADAPTER_INTERFACE_H_ */
+bt_status_t bt_sal_le_enable_key_derivation(bt_controller_id_t id, bool brkey_to_lekey, bool lekey_to_brkey);
+
+struct bt_conn* get_le_conn_from_addr(bt_address_t* addr);
+bt_status_t get_le_addr_from_conn(struct bt_conn* conn, bt_address_t* addr);
+
+#endif /* __SAL_ADAPTER_LE_INTERFACE_H_ */
