dc64541a30376d399724c37e05d55bfaf7615b8c
zhangyuan20 <zhangyuan20@xiaomi.com>
2025-01-08 21:51:34 +0800

    [bugfix]bluetooth/framework: callback disconnected state when shutdown
    bug:v/52429
    
    Rootcause: When disable bluetooth, some profiles can not callback to upper layer,it will result some error, so when shutdown profiles, callback state first
    
    Signed-off-by: zhangyuan20 <zhangyuan20@xiaomi.com>

--- service/profiles/hid/hid_device_service.c
+++ service/profiles/hid/hid_device_service.c
@@ -47,6 +47,7 @@
 
 typedef struct {
     bool started;
+    bool le_hid;
     hid_app_state_t app_state;
     bt_address_t peer_addr;
     profile_connection_state_t conn_state;
@@ -128,7 +129,7 @@ typedef struct {
 /****************************************************************************
  * Private Data
  ****************************************************************************/
-static hid_device_handle_t g_hidd_handle = { .started = false };
+static hid_device_handle_t g_hidd_handle = { .started = false, .le_hid = false };
 
 /****************************************************************************
  * Private Functions
@@ -267,12 +268,16 @@ static bt_status_t hid_device_shutdown(profile_on_shutdown_t cb)
         return BT_STATUS_NOT_ENABLED;
     }
 
+    if (g_hidd_handle.conn_state != PROFILE_STATE_DISCONNECTED)
+        HIDD_CALLBACK_FOREACH(g_hidd_handle.callbacks, connection_state_cb, &g_hidd_handle.peer_addr, g_hidd_handle.le_hid, PROFILE_STATE_DISCONNECTED);
+
     if (g_hidd_handle.conn_state == PROFILE_STATE_CONNECTED || g_hidd_handle.conn_state == PROFILE_STATE_CONNECTING) {
         bt_sal_hid_device_disconnect(&g_hidd_handle.peer_addr);
         bt_pm_conn_close(PROFILE_HID_DEV, &g_hidd_handle.peer_addr);
     }
 
     g_hidd_handle.started = false;
+    g_hidd_handle.le_hid = false;
     g_hidd_handle.app_state = HID_APP_STATE_NOT_REGISTERED;
     g_hidd_handle.conn_state = PROFILE_STATE_DISCONNECTED;
     bt_addr_set_empty(&g_hidd_handle.peer_addr);
@@ -351,6 +356,8 @@ static bt_status_t hid_device_register_app(hid_device_sdp_settings_t* sdp, bool 
     if (status == BT_STATUS_SUCCESS) {
         g_hidd_handle.app_state = HID_APP_STATE_REGISTERED;
     }
+
+    g_hidd_handle.le_hid = le_hid;
 
 exit:
     pthread_mutex_unlock(&g_hidd_handle.hid_lock);
