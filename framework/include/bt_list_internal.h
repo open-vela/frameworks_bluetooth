ce18ec2afa229927b3af74b517bdede3a7f5d934
Zihao Gao <gaozihao@xiaomi.com>
2025-03-28 18:21:14 +0800

    Bluetooth: defer LE scan results if IPC blocked.
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- framework/include/bt_list_internal.h
+++ framework/include/bt_list_internal.h
@@ -137,6 +137,15 @@
         list_initialize(item); \
     } while (0)
 
+#define list_merge(list_dst, list_src)             \
+    do {                                           \
+        (list_dst)->prev->next = (list_src)->next; \
+        (list_src)->next->prev = (list_dst)->prev; \
+        (list_src)->prev->next = (list_dst);       \
+        (list_dst)->prev = (list_src)->prev;       \
+        list_initialize(list_src);                 \
+    } while (0)
+
 #define list_remove_head_type(list, type, member)              \
     ({                                                         \
         FAR struct list_node* __node = list_remove_head(list); \
