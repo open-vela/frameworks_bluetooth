6770f271020aafc20e53edadd0dc4980aaf316a8
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2025-04-18 17:16:50 +0800

    Adjust the priority of bt_client to always be 1 higher than that of bluetoothd.
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- framework/common/uv_thread_loop.c
+++ framework/common/uv_thread_loop.c
@@ -179,7 +179,7 @@ int thread_loop_run(uv_loop_t* loop, bool start_thread, const char* name)
         uv_thread_options_t options = {
             UV_THREAD_HAS_STACK_SIZE | UV_THREAD_HAS_PRIORITY,
             LOOP_THREAD_STACK_SIZE,
-            CONFIG_BLUETOOTH_SERVICE_LOOP_THREAD_PRIORITY
+            CONFIG_BLUETOOTH_SERVICE_LOOP_THREAD_PRIORITY + 1
         };
         ret = uv_thread_create_ex(&priv->thread, &options, thread_schedule_loop, (void*)loop);
         if (ret != 0) {
