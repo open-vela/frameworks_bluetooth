d21ed1d54fa26bb27b95628aac9e5dbf505a6e88
Lu Jia <jialu@xiaomi.com>
2025-05-14 22:56:58 +0800

    bluetooth: add basic sample code
    Signed-off-by: jialu <jialu@xiaomi.com>

---
+++ sample_code/basic/app_bt_gap.c
@@ -0,0 +1,24 @@
+/****************************************************************************
+ *  Copyright (C) 2025 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "basic.h"
+
+void app_bt_gap_handle_message(bt_instance_t* g_bt_ins, node_t* node)
+{
+    // Handle Bluetooth message，Call the API interface of Bluetooth.
+}
