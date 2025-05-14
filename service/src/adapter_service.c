322dd6b2641ccee1797a9af56d6a81c239ea30b9
chejinxian1 <chejinxian1@xiaomi.com>
2025-05-14 22:56:58 +0800

    Adapter: Modify the timing of entering bonding state.
    Rootcause: After agreeing to exchange IO capabilities, the upper layer was notified to enter the bonding state, but the Framework did not actually enter bonding. The purpose of entering bonding here is to avoid the situation where insufficient IO capabilities prevent the corresponding information from being obtained from the protocol stack to enter bonding.
    
    Signed-off-by: chejinxian1 <chejinxian1@xiaomi.com>

--- service/src/adapter_service.c
+++ service/src/adapter_service.c
@@ -2810,9 +2810,9 @@ bt_status_t adapter_pair_request_reply(bt_address_t* addr, bool accept)
     bt_status_t status;
     status = bt_sal_pair_reply(PRIMARY_ADAPTER, addr, accept ? 0 : HCI_ERR_PAIRING_NOT_ALLOWED);
     if (status == BT_STATUS_SUCCESS && accept) {
-        /* callback bonding */
-        CALLBACK_FOREACH(CBLIST, adapter_callbacks_t, on_bond_state_changed,
-            addr, BT_TRANSPORT_BREDR, BOND_STATE_BONDING, false);
+        adapter_lock();
+        device_set_bond_state(device, BOND_STATE_BONDING, false, adapter_notify_bond_state);
+        adapter_unlock();
     }
 
     return status;
