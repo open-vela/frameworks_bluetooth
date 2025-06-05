e07ce6d5ae83eaa4a3cd19dfdfaa1e95c7a97ec4
zhangyuan20 <zhangyuan20@xiaomi.com>
2025-06-05 21:16:02 +0800

    zephyr-pts: add register notification interface for avrcp ct
    bug: v/58789
    
    Signed-off-by: zhangyuan20 <zhangyuan20@xiaomi.com>

--- framework/socket/bt_avrcp_control.c
+++ framework/socket/bt_avrcp_control.c
@@ -166,3 +166,21 @@ bt_status_t bt_avrcp_control_get_playback_state(bt_instance_t* ins, bt_address_t
 
     return packet.avrcp_control_r.status;
 }
+
+bt_status_t bt_avrcp_control_register_notification(bt_instance_t* ins, bt_address_t* addr, uint8_t event, uint32_t interval)
+{
+    bt_message_packet_t packet;
+    bt_status_t status;
+
+    BT_SOCKET_INS_VALID(ins, BT_STATUS_PARM_INVALID);
+
+    memcpy(&packet.avrcp_control_pl._bt_avrcp_control_register_notification.addr, addr, sizeof(packet.avrcp_control_pl._bt_avrcp_control_register_notification.addr));
+    packet.avrcp_control_pl._bt_avrcp_control_register_notification.event = event;
+    packet.avrcp_control_pl._bt_avrcp_control_register_notification.interval = interval;
+    status = bt_socket_client_sendrecv(ins, &packet, BT_AVRCP_CT_REGISTER_NOTIFICATION_CMD);
+    if (status != BT_STATUS_SUCCESS) {
+        return status;
+    }
+
+    return packet.avrcp_control_r.status;
+}
