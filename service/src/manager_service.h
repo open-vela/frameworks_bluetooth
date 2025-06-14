a3f1dafa7219b9ee677e9bfb452856de6bd7a2e4
Lu Jia <jialu@xiaomi.com>
2025-06-14 21:29:14 +0800

    bluetooth: Add bluetooth_get_async_instance()
    bug: v/63432
    
    Signed-off-by: jialu <jialu@xiaomi.com>

--- service/src/manager_service.h
+++ service/src/manager_service.h
@@ -27,7 +27,11 @@ void manager_cleanup(void);
 bt_status_t manager_create_instance(uint64_t handle, uint32_t type,
     const char* name, pid_t pid, uid_t uid,
     uint32_t* app_id);
+bt_status_t manager_create_async_instance(uint64_t handle, uint32_t type,
+    const char* name, pid_t pid, uid_t uid,
+    uint32_t* app_id);
 bt_status_t manager_get_instance(const char* name, pid_t pid, uint64_t* handle);
+bt_status_t manager_get_async_instance(const char* name, pid_t pid, uint64_t* handle);
 bt_status_t manager_delete_instance(uint32_t app_id);
 bt_status_t manager_start_service(uint32_t app_id, enum profile_id profile);
 bt_status_t manager_stop_service(uint32_t app_id, enum profile_id profile);
