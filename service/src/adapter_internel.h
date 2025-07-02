a1857d58e1fd1832ee95600eb7c307a0c270e32a
liuxiang18 <liuxiang18@xiaomi.com>
2025-07-02 10:21:04 +0800

    bluetooth: fix for addr_type not being reported.
    VELAPLATFO-62114
    
    No address type is currently reported, resulting in all device instances having a default address type: 0(public addr type).
    
    Change-Id: Idd41c725a9f4ea3c39c1ec4e0eec34762d57816c
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/src/adapter_internel.h
+++ service/src/adapter_internel.h
@@ -255,6 +255,7 @@ void adapter_on_le_local_oob_data_got(bt_address_t* addr, bt_128key_t c_val, bt_
 
 /* adapter sal invoke functions */
 bt_address_t* adapter_get_le_remote_address(bt_address_t* addr, ble_addr_type_t addr_type);
+ble_addr_type_t adapter_get_le_remote_address_type(bt_address_t* addr);
 
 /* adapter framework invoke functions */
 void adapter_init(void);
