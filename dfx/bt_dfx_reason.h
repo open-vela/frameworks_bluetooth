18c11e52e52875b078877b811e3694f10ecf2b30
Lu Jia <jialu@xiaomi.com>
2025-07-07 19:42:39 +0800

    bluetooth: Increase BR gap data analytics with dfx.
    Signed-off-by: jialu <jialu@xiaomi.com>

---
+++ dfx/bt_dfx_reason.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+ *  Copyright (C) 2025 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+#ifndef _BT_DFX_REASON_H_
+#define _BT_DFX_REASON_H_
+
+// error reason
+#define BT_DFXE_REPEATED_ATTEMPT "btRepeatedAttempt"
+#define BT_DFXE_ADAPTER_STATE_NOT_ON "btAdapterStateNotOn"
+#define BT_DFXE_PAGE_TIMEOUT "btPageTimeout"
+#define BT_DFXE_CONN_TIMEOUT "btConnTimeout"
+#define BT_DFXE_CONN_FAILED_TO_BE_ESTABLISHED "btConnFailedToBeEstablished"
+
+#endif /* _BT_DFX_REASON_H_ */
