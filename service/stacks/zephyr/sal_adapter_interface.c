a737850e1f8018f32852780b1977aefa0dea6d96
v-yichenxi <v-yichenxi@xiaomi.com>
2025-07-09 14:02:25 +0800

    bluetooth: fix visibility not disabled before bt_disable
    bug: v/64223
    
    rootcause: device remained visible after bt_disable
    
    Signed-off-by: v-yichenxi <v-yichenxi@xiaomi.com>

--- service/stacks/zephyr/sal_adapter_interface.c
+++ service/stacks/zephyr/sal_adapter_interface.c
@@ -563,6 +563,7 @@ static void STACK_CALL(brder_disable)(void* args)
     UNUSED(args);
 
     zblue_unregister_callback();
+    bt_br_set_visibility(false, false);
 #ifndef CONFIG_BLUETOOTH_BLE_SUPPORT
     bt_br_set_visibility(false, false);
     bt_disable();
