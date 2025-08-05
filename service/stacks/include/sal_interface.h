14eff59179dba01e08316abf97979c6385ba35a6
Kai Cheng <chengkai@xiaomi.com>
2025-08-05 16:47:57 +0800

    bluetooth: merged bt driver thread to btservice loop thread
    bug: v/76633
    
    remove bt driver thread, and merger it to service loop thread
    
    Signed-off-by: Kai Cheng <chengkai@xiaomi.com>

--- service/stacks/include/sal_interface.h
+++ service/stacks/include/sal_interface.h
@@ -30,6 +30,8 @@
 #endif
 #include "sal_debug_interface.h"
 
+#include "hci_h4.h"
+
 #if defined(CONFIG_BLUETOOTH_STACK_BREDR_BLUELET) || defined(CONFIG_BLUETOOTH_STACK_LE_BLUELET)
 #include "sal_adapter_interface.h"
 #endif
