17e21a0c5f7a4e895c2b0034e30578007d1890e4
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2025-09-11 18:25:08 +0800

    support user response for descriptor read requests
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- framework/include/bt_gatt_defs.h
+++ framework/include/bt_gatt_defs.h
@@ -152,7 +152,11 @@ typedef enum {
 
 /* GATT_H_DESCRIPTOR */
 #define GATT_H_CCCD(_perm, _change, _handle) \
-    GATT_H_DESCRIPTOR(BT_UUID_DECLARE_16(0x2902), _perm, ATTR_RSP_BY_APP, NULL, _change, NULL, 0, _handle)
+    GATT_H_DESCRIPTOR(BT_UUID_DECLARE_16(0x2902), _perm, ATTR_AUTO_RSP, NULL, _change, NULL, 0, _handle)
+
+/* GATT_H_DESCRIPTOR for CCCD with user response (APP handles read/write) */
+#define GATT_H_CCCD_USER_RSP(_perm, _read, _write, _handle) \
+    GATT_H_DESCRIPTOR(BT_UUID_DECLARE_16(0x2902), _perm, ATTR_RSP_BY_APP, _read, _write, NULL, 0, _handle)
 
 /* GATT_H_DESCRIPTOR */
 #define GATT_H_CPFD(_value, _length, _handle) \
