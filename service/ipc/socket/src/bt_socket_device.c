11aac1924c407edcc255b6d464b8d0cb7822171a
liuxiang18 <liuxiang18@xiaomi.com>
2025-09-12 17:52:48 +0800

    bluetooth: implement bt_device_set_bondable_le API to change bond mode.
    bug: v/67335
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/ipc/socket/src/bt_socket_device.c
+++ service/ipc/socket/src/bt_socket_device.c
@@ -306,6 +306,10 @@ void bt_socket_server_device_process(service_poll_t* poll,
                 packet->devs_pl._bt_device_set_security_level.level,
                 packet->devs_pl._bt_device_set_security_level.transport);
             break;
+        case BT_DEVICE_SUBCODE_SET_BONDABLE_LE:
+            packet->devs_r.status = BTSYMBOLS(bt_device_set_bondable_le)(ins,
+                packet->devs_pl._bt_device_set_bondable_le.accept);
+            break;
         default:
             packet->devs_r.status = BT_STATUS_NOT_SUPPORTED;
             break;
