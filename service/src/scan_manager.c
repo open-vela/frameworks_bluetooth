67c0a67d45db399a059fd948f62c3f13286485c9
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2025-09-26 14:49:08 +0800

    bluetooth: fix the heap use after free bug in scan_manager.
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- service/src/scan_manager.c
+++ service/src/scan_manager.c
@@ -310,13 +310,11 @@ static uint32_t register_scanner(scanner_t* scanner)
         return BT_SCAN_STATUS_START_FAIL;
 
     if (scanner_manager.scanner_cnt == CONFIG_BLUETOOTH_LE_SCANNER_MAX_NUM) {
-        delete_scanner(scanner);
         return BT_SCAN_STATUS_SCANNER_REG_NOMEM;
     }
 
     for (i = 0; i < CONFIG_BLUETOOTH_LE_SCANNER_MAX_NUM; i++) {
         if (scanner_manager.scanner_list[i] != NULL && scanner_compare(scanner_manager.scanner_list[i], scanner)) {
-            delete_scanner(scanner);
             return BT_SCAN_STATUS_SCANNER_EXISTED;
         }
     }
@@ -409,6 +407,7 @@ static void start_scan(void* data)
     uint32_t status = register_scanner(scanner);
     if (status != BT_SCAN_STATUS_SUCCESS) {
         scanner->callbacks->on_scan_start_status(get_remote(scanner), status);
+        delete_scanner(scanner);
         goto ret;
     }
 
@@ -420,6 +419,7 @@ static void start_scan(void* data)
         bt_sal_le_set_scan_parameters(PRIMARY_ADAPTER, &params);
         if (bt_sal_le_start_scan(PRIMARY_ADAPTER) != BT_STATUS_SUCCESS) {
             scanner->callbacks->on_scan_start_status(get_remote(scanner), BT_SCAN_STATUS_START_FAIL);
+            delete_scanner(scanner);
             goto ret;
         }
         scanner_manager.is_scanning = true;
