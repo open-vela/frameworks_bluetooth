9e84c17a799fbce2753f134699b7543288fdbb12
wuxiaodong6 <wuxiaodong6@xiaomi.com>
2025-11-06 15:44:50 +0800

    fix sal_(gattc & le_adv & le_scan) _interface memory leaks
    Rootcause: the memory may not be released.
    
    Signed-off-by: wuxiaodong6 <wuxiaodong6@xiaomi.com>
    Signed-off-by: miot-robot <miot-robot@xiaomi.com>

--- service/stacks/zephyr/sal_gatt_client_interface.c
+++ service/stacks/zephyr/sal_gatt_client_interface.c
@@ -329,6 +329,7 @@ static bt_status_t sal_send_req(sal_adapter_req_t* req)
 
     if (!service_loop_work((void*)req, sal_invoke_async, NULL)) {
         BT_LOGE("%s, service_loop_work failed", __func__);
+        free(req);
         return BT_STATUS_FAIL;
     }
 
