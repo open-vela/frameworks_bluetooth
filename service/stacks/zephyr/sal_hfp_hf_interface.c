c6f9905303c28f0737af3766361eb50633f90008
YuhengLi <liyuheng@xiaomi.com>
2025-11-20 14:54:10 +0800

    zblue: hfp: implement bt_sal_hfp_hf_connect_audio API
    VELAPLATFO-80257
    
    Change-Id: I389d4fc12f17bf629788ffd5d7df263fdbdf665f
    Signed-off-by: YuhengLi <liyuheng@xiaomi.com>

--- service/stacks/zephyr/sal_hfp_hf_interface.c
+++ service/stacks/zephyr/sal_hfp_hf_interface.c
@@ -788,7 +788,19 @@ bt_status_t bt_sal_hfp_hf_disconnect(bt_address_t* addr)
 
 bt_status_t bt_sal_hfp_hf_connect_audio(bt_address_t* addr)
 {
-    return BT_STATUS_UNSUPPORTED;
+    bt_hfp_hf_connection_t* sal_conn = find_connection_by_addr(addr);
+    if (!sal_conn) {
+        BT_LOGE("%s, Failed to find connection", __func__);
+        return BT_STATUS_FAIL;
+    }
+
+    int ret = Z_API(bt_hfp_hf_audio_connect)(sal_conn->hf);
+    if (ret == -ENOTSUP) {
+        return BT_STATUS_UNSUPPORTED;
+    }
+
+    SAL_CHECK_RET(ret, 0);
+    return BT_STATUS_SUCCESS;
 }
 
 bt_status_t bt_sal_hfp_hf_disconnect_audio(bt_address_t* addr)
