76e949e89738128796172817045f4a0535165fbf
liuxiang18 <liuxiang18@xiaomi.com>
2025-11-27 23:23:23 +0800

    bluetooth: Expose a subset of the universal interfaces for usage in storage updates.
    bug: v/71112
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/common/storage.h
+++ service/common/storage.h
@@ -32,4 +32,32 @@ int bt_storage_load_bonded_device(load_storage_callback_t cb);
 int bt_storage_load_whitelist_device(load_storage_callback_t cb);
 int bt_storage_load_le_bonded_device(load_storage_callback_t cb);
 
+#ifdef CONFIG_BLUETOOTH_STORAGE_PROPERTY_SUPPORT
+#define GEN_PROP_KEY(buf, key, address, len) snprintf((buf), (len), "%s%02X:%02X:%02X:%02X:%02X:%02X", \
+    (key),                                                                                             \
+    (address)->addr[5], (address)->addr[4], (address)->addr[3],                                        \
+    (address)->addr[2], (address)->addr[1], (address)->addr[0])
+
+#define PARSE_PROP_KEY(addr_str, name, name_prefix_len, addr_str_len, addr_ptr) \
+    do {                                                                        \
+        strlcpy((addr_str), (name) + (name_prefix_len), (addr_str_len));        \
+        bt_addr_str2ba((addr_str), (addr_ptr));                                 \
+    } while (0)
+
+#define ERROR_ADAPTERINFO_VALUE -1
+
+#define BT_KVDB_ADAPTERINFO_NAME "persist.bluetooth.adapterInfo.name"
+#define BT_KVDB_ADAPTERINFO_COD "persist.bluetooth.adapterInfo.class_of_device"
+#define BT_KVDB_ADAPTERINFO_IOCAP "persist.bluetooth.adapterInfo.io_capability"
+#define BT_KVDB_ADAPTERINFO_SCAN "persist.bluetooth.adapterInfo.scan_mode"
+#define BT_KVDB_ADAPTERINFO_BOND "persist.bluetooth.adapterInfo.bondable"
+
+#define BT_KVDB_ADAPTERINFO "persist.bluetooth.adapterInfo."
+#define BT_KVDB_BTBOND "persist.bluetooth.btbonded."
+#define BT_KVDB_BLEBOND "persist.bluetooth.blebonded."
+#define BT_KVDB_BLEWHITELIST "persist.bluetooth.whitelist."
+
+void bt_storage_delete(char* key, uint16_t items, char* prop_name);
+#endif
+
 #endif /* _BT_STORAGE_H__ */
