2ba4efa4ed954ec7d9d6d27dc954e9e793783b7e
YuhengLi <liyuheng@xiaomi.com>
2025-12-01 16:14:29 +0800

    zblue: hfp: hf: redial when bt_sal_hfp_hf_dial_number was called with a NULL number
    Signed-off-by: YuhengLi <liyuheng@xiaomi.com>

--- service/stacks/zephyr/sal_hfp_hf_interface.c
+++ service/stacks/zephyr/sal_hfp_hf_interface.c
@@ -1079,6 +1079,16 @@ bt_status_t bt_sal_hfp_hf_hangup_call(bt_address_t* addr)
 bt_status_t bt_sal_hfp_hf_dial_number(bt_address_t* addr, const char* number)
 {
     bt_hfp_hf_connection_t* sal_conn = find_connection_by_addr(addr);
+    if (!sal_conn) {
+        BT_LOGE("%s, Failed to find connection", __func__);
+        return BT_STATUS_PARM_INVALID;
+    }
+
+    if (!number) {
+        SAL_CHECK_RET(Z_API(bt_hfp_hf_redial)(sal_conn->hf), 0);
+        return BT_STATUS_SUCCESS;
+    }
+
     SAL_CHECK_RET(Z_API(bt_hfp_hf_number_call)(sal_conn->hf, number), 0);
     return BT_STATUS_SUCCESS;
 }
