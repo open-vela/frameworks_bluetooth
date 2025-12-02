37658bf70f0314e19308a9159034c8479b9e6c74
Lu Jia <jialu@xiaomi.com>
2025-12-02 10:19:18 +0800

    bluetooth: Fix the issue of failed GATTC deletion.
    VELAPLATFO-79667
    
    When deleting a GATTC instance, the Advanced API identifies clients based on their device address. If an application creates two GATTC instances with the same device address, and attempts to delete the second instance before the server has returned the asynchronous callback for the first deletion, both deletion operations will effectively target the same underlying GATTC instance due to the identical device address.
    
    Change-Id: Id64ba1500f62068c3cefd6b5db8bca9f78ae6e18
    Signed-off-by: jialu <jialu@xiaomi.com>
    Signed-off-by: miot-robot <miot-robot@xiaomi.com>

--- feature/feature_async/src/bluetooth_ble_impl.c
+++ feature/feature_async/src/bluetooth_ble_impl.c
@@ -1640,7 +1640,7 @@ static void feature_gattc_destroy(FeatureInterfaceHandle handle)
             bt_gattc_feature_disconnect_async(gattc_info->gattc->handle, NULL, NULL);
         }
 
-        bt_gattc_feature_delete_client_async(bluetooth_instance, &gattc_info->gattc->remote_address, NULL, NULL);
+        bt_gattc_feature_delete_client_async(bluetooth_instance, gattc_info->gattc->handle, NULL, NULL);
     }
 
     free(gattc_info->gattc);
