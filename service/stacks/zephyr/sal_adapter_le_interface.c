49e9c462d163dc7906d520dda5a569b8b6309392
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2025-12-16 16:21:29 +0800

    always report BONDED on pairing_complete callbacks
    VELAPLATFO-80684
    
    Treat pairing_complete as a successful bonding result and always report
    BOND_STATE_BONDED to upper layers for both BR/EDR and BLE.
    
    The business logic doesn't need to distinguish the no_bonding state.
    This behavior is consistent with the legacy Bluetooth stack.
    
    Also remove bond state reporting from link_key_notify and rely on
    pairing_complete to deliver bond state updates.
    
    Change-Id: I9aa1573464782019d85af837a5eb3ed96db8e962
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- service/stacks/zephyr/sal_adapter_le_interface.c
+++ service/stacks/zephyr/sal_adapter_le_interface.c
@@ -84,7 +84,7 @@ static void zblue_on_disconnected(struct bt_conn* conn, uint8_t reason);
 static void zblue_on_security_changed(struct bt_conn* conn, bt_security_t level, enum bt_security_err err);
 static void zblue_on_pairing_complete_ctkd(struct bt_conn* conn, bool is_link_key);
 #endif
-static void zblue_on_pairing_complete(struct bt_conn* conn, bool bonded);
+static void zblue_on_pairing_complete(struct bt_conn* conn, bool bonding_flag);
 static void zblue_on_pairing_failed(struct bt_conn* conn, enum bt_security_err reason);
 static void zblue_on_bond_deleted(uint8_t id, const bt_addr_le_t* peer);
 static void zblue_convert_le_addr(bt_address_t* addr, ble_addr_type_t type, bt_addr_le_t* le_addr);
@@ -709,25 +709,22 @@ static void zblue_on_pairing_complete_ctkd(struct bt_conn* conn, bool is_link_ke
 #endif
 }
 
-static void zblue_on_pairing_complete(struct bt_conn* conn, bool bonded)
+static void zblue_on_pairing_complete(struct bt_conn* conn, bool bonding_flag)
 {
     bt_address_t addr;
-    bond_state_t state;
 
-    BT_LOGD("%s", __func__);
+    if (!bt_conn_get_dst(conn)) {
+        return;
+    }
 
     if (get_le_addr_from_conn(conn, &addr) != BT_STATUS_SUCCESS) {
         BT_LOGE("%s, get_le_addr_from_conn failed", __func__);
         return;
     }
 
-    if (bonded) {
-        state = BOND_STATE_BONDED;
-    } else {
-        state = BOND_STATE_NONE;
-    }
+    BT_LOGD("%s bonding_flag: %s", __func__, bonding_flag ? "true" : "false");
 
-    adapter_on_bond_state_changed(&addr, state, BT_TRANSPORT_BLE, BT_STATUS_SUCCESS, false);
+    adapter_on_bond_state_changed(&addr, BOND_STATE_BONDED, BT_TRANSPORT_BLE, BT_STATUS_SUCCESS, false);
 }
 
 static void zblue_on_pairing_failed(struct bt_conn* conn, enum bt_security_err reason)
