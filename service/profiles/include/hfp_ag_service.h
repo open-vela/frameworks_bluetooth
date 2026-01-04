0cc3996507050b97d32a27de17cb20ab39cdd942
YuhengLi <liyuheng@xiaomi.com>
2026-01-04 15:57:37 +0800

    Android-Vela:add cind interface for android
    bug: v/59050
    
    Rootcause:When reconnect to the headset during a call, the headset will obtain the call status through the cind command. Since Vela does not have modem, the status will be error. So, get cind from Android.
    
    Signed-off-by: zhangyuan20 <zhangyuan20@xiaomi.com>

--- service/profiles/include/hfp_ag_service.h
+++ service/profiles/include/hfp_ag_service.h
@@ -86,6 +86,7 @@ void ag_service_notify_call_dial(bt_address_t* addr, const char* number);
 void ag_service_notify_cmd_received(bt_address_t* addr, const char* at_cmd);
 void ag_service_notify_vendor_specific_cmd(bt_address_t* addr, const char* command, uint16_t company_id, const char* value);
 void ag_service_notify_clcc_cmd(bt_address_t* addr);
+void ag_service_notify_cind_cmd(bt_address_t* addr);
 
 /*
  * telephony
@@ -131,6 +132,9 @@ typedef struct ag_interface {
     bt_status_t (*send_clcc_response)(bt_address_t* addr, uint32_t index, hfp_call_direction_t dir,
         hfp_ag_call_state_t state, hfp_call_mode_t mode, hfp_call_mpty_type_t mpty,
         hfp_call_addrtype_t type, const char* number);
+    bt_status_t (*send_cind_response)(bt_address_t* addr, hfp_network_state_t network, hfp_call_t call,
+        hfp_callheld_t call_held, hfp_callsetup_t call_setup, uint8_t signal,
+        hfp_roaming_state_t roam, uint8_t battery);
 } hfp_ag_interface_t;
 
 /*
