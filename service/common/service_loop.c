92c037fc395981358a411a38b2a755113e5652bf
v-chenghuijin <v-chenghuijin@xiaomi.com>
2026-01-09 10:44:06 +0800

    Revert "Bluetooth: Prevent crash when calling uv_run after event loop closure."
    VELAPLATFO-80385
    
    This reverts commit 1974e4be0eba5f529868b7948032fb8317447471.
    
    Change-Id: Iace2429a83376470359e6cef7c01d5c1af849fe1
    Signed-off-by: v-chenghuijin <v-chenghuijin@xiaomi.com>

--- service/common/service_loop.c
+++ service/common/service_loop.c
@@ -287,10 +287,8 @@ void service_loop_exit(void)
         uv_sem_wait(&loop->exited);
         uv_sem_destroy(&loop->exited);
     } else {
-        if (!uv_loop_is_close(handle)) {
-            uv_run(handle, UV_RUN_ONCE);
-            (void)uv_loop_close(handle);
-        }
+        uv_run(handle, UV_RUN_ONCE);
+        (void)uv_loop_close(handle);
     }
 
     uv_mutex_lock(&loop->msg_lock);
