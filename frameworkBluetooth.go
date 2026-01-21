d6cd380e96f7170cae4b1adc6a5ea7fd21500e66
zhangyuan20 <zhangyuan20@xiaomi.com>
2026-01-21 16:27:31 +0800

    bluetooth: add android15 config control.
    bug: v/84700
    
    Rootcause: Some configurations are not enabled in Android 15 and require the addition of macro controls in Android 15.
    
    Signed-off-by: zhangyuan20 <zhangyuan20@xiaomi.com>

--- frameworkBluetooth.go
+++ frameworkBluetooth.go
@@ -33,6 +33,8 @@ func frameworkBluetoothHook(ctx android.LoadHookContext) {
         p.Cflags = append(p.Cflags, "-DANDROID_12")
     } else if (Version == "14") {
         p.Cflags = append(p.Cflags, "-DANDROID_14")
+    } else if (Version == "15") {
+        p.Cflags = append(p.Cflags, "-DANDROID_15")
     }
 
     ctx.AppendProperties(p)
