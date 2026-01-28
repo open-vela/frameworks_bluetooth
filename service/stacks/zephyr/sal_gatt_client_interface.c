d3073fcd39e969cd5fd1ea2736649921de4cdd76
liuxiang18 <liuxiang18@xiaomi.com>
2026-01-28 09:48:31 +0800

    gatts:Implement the bt_sal_gatt_server_connect_bear interface
    VELAPLATFO-84474
    
    add att_over_br connected and disconnected callback, and replace le_conn_xx as bt_conn_xx
    
    
    Change-Id: I15cb62f0c3890f5925998015d317e6107a197d17
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/stacks/zephyr/sal_gatt_client_interface.c
+++ service/stacks/zephyr/sal_gatt_client_interface.c
@@ -347,7 +347,7 @@ static void STACK_CALL(conn_connect)(void* args)
     struct bt_conn* conn = NULL;
     int err;
 
-    if (le_conn_set_role(&req->addr, GATT_ROLE_CLIENT) != BT_STATUS_SUCCESS) {
+    if (bt_conn_set_role(BT_TRANSPORT_BLE, &req->addr, GATT_ROLE_CLIENT) != BT_STATUS_SUCCESS) {
         return;
     }
 
@@ -356,7 +356,7 @@ static void STACK_CALL(conn_connect)(void* args)
 
     err = bt_conn_le_create(&address, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT, &conn);
     if (err) {
-        le_conn_remove(&req->addr);
+        bt_conn_remove(&req->addr, BT_TRANSPORT_BLE);
         BT_LOGE("%s, failed to create connection (%d)", __func__, err);
         return;
     }
