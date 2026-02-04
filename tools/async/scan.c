6f1037af1d1058262864437e49aef2b0ae890723
huangyulong3 <huangyulong3@xiaomi.com>
2026-02-04 09:33:23 +0800

    Memory Optimization: Change the bluetooth bt_tool scan module to const for reduce the ram size.
    bug:v/84968
    
    Rootcause: To reduce RAM resource consumption, the `scan_options`,
    in the bt_tools scan module must be marked with the `const` modifier.
    This ensures it is compiled and linked into Flash memory instead of RAM.
    
    Signed-off-by: huangyulong3 <huangyulong3@xiaomi.com>

--- tools/async/scan.c
+++ tools/async/scan.c
@@ -34,7 +34,7 @@ static int dump_scan_cmd(void* handle, int argc, char* argv[]);
 
 static bt_scanner_t* g_scanner = NULL;
 
-static struct option scan_options[] = {
+static const struct option scan_options[] = {
     { "type", required_argument, 0, 't' },
     { "phy", required_argument, 0, 'p' },
     { "mode", required_argument, 0, 'm' },
@@ -43,7 +43,7 @@ static struct option scan_options[] = {
     { 0, 0, 0, 0 }
 };
 
-static bt_command_t g_scanner_async_tables[] = {
+static const bt_command_t g_scanner_async_tables[] = {
     { "start", start_scan_cmd, 0, "start scan\n"
                                   "\t  -t or --type, le scan type (0: passive, 1: active)\n"
                                   "\t  -p or --phy, le scan phy (1M/2M/Coded)\n"
@@ -226,7 +226,7 @@ int scan_command_exec_async(void* handle, int argc, char* argv[])
     int ret = CMD_USAGE_FAULT;
 
     if (argc > 0)
-        ret = execute_command_in_table_offset(handle, g_scanner_async_tables,
+        ret = execute_command_in_table_offset(handle, (bt_command_t *)g_scanner_async_tables,
             ARRAY_SIZE(g_scanner_async_tables),
             argc, argv, 0);
 
