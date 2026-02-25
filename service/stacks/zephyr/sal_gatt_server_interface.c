e1d922be22f687b39c3aa816a61a8d8adbccf2ae
liuxiang18 <liuxiang18@xiaomi.com>
2026-02-25 19:30:20 +0800

    gatts: add judge about conn_info is NULL.
    bug: v/84474
    
    When the SAL layer does not store the BREDR connection information, it returns info as NULL. At this point, attempting an erroneous address offset on info->conn will result in a null pointer return.
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/stacks/zephyr/sal_gatt_server_interface.c
+++ service/stacks/zephyr/sal_gatt_server_interface.c
@@ -1297,7 +1297,7 @@ bt_status_t bt_sal_gatt_server_send_response(bt_controller_id_t id, bt_address_t
         BT_LOGW("%s, le conn null", __func__);
 
         info = bt_conn_find(addr, BT_TRANSPORT_BREDR);
-        conn = info->conn;
+        conn = info ? info->conn : NULL;
         if (!conn) {
             BT_LOGE("%s, br conn null", __func__);
             return BT_STATUS_NOT_FOUND;
@@ -1385,7 +1385,7 @@ bt_status_t bt_sal_gatt_server_send_notification(bt_controller_id_t id, bt_addre
         BT_LOGW("%s, le conn null", __func__);
 
         info = bt_conn_find(addr, BT_TRANSPORT_BREDR);
-        context.conn = info->conn;
+        context.conn = info ? info->conn : NULL;
         if (!context.conn) {
             BT_LOGE("%s, br conn null", __func__);
             return BT_STATUS_FAIL;
@@ -1482,7 +1482,7 @@ bt_status_t bt_sal_gatt_server_send_indication(bt_controller_id_t id, bt_address
         BT_LOGW("%s, le conn null", __func__);
 
         info = bt_conn_find(addr, BT_TRANSPORT_BREDR);
-        context.conn = info->conn;
+        context.conn = info ? info->conn : NULL;
         if (!context.conn) {
             BT_LOGE("%s, br conn null", __func__);
             return BT_STATUS_FAIL;
