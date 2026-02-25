4762d1271d95fc0a2f7061756cdeb89f8837f277
liuxiang18 <liuxiang18@xiaomi.com>
2026-02-25 19:30:20 +0800

    gatts: fix an issue where `bt_conn_unref` was not called after using `bt_conn_lookup_addr_br`.
    bug: v/84474
    
    1. Add a call to bt_conn_unref(conn)
    2. Use the locally stored conn in the sal layer to avoid executing `bt_conn_unref`
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- service/stacks/zephyr/sal_gatt_server_interface.c
+++ service/stacks/zephyr/sal_gatt_server_interface.c
@@ -768,6 +768,7 @@ static bt_status_t do_gatts_disconnect(bt_controller_id_t id, bt_address_t* bd_a
     }
 
     err = bt_att_br_disconnect(conn);
+    bt_conn_unref(conn);
     if (err) {
         BT_LOGE("%s, disconnect fail err:%d", __func__, err);
         return BT_STATUS_FAIL;
@@ -1366,9 +1367,11 @@ bt_status_t bt_sal_gatt_server_send_notification(bt_controller_id_t id, bt_addre
     /* FIXME: If the LE address matches the BREDR address, only the LE connection will be notified. */
     context.conn = get_le_conn_from_addr(addr);
     if (!context.conn) {
+        bt_conn_info_t* info;
         BT_LOGW("%s, le conn null", __func__);
 
-        context.conn = bt_conn_lookup_addr_br((bt_addr_t*)addr);
+        info = bt_conn_find(addr, BT_TRANSPORT_BREDR);
+        context.conn = info->conn;
         if (!context.conn) {
             BT_LOGE("%s, br conn null", __func__);
             return BT_STATUS_FAIL;
@@ -1461,9 +1464,11 @@ bt_status_t bt_sal_gatt_server_send_indication(bt_controller_id_t id, bt_address
     /* FIXME: If the LE address matches the BREDR address, only the LE connection will be indicated. */
     context.conn = get_le_conn_from_addr(addr);
     if (!context.conn) {
+        bt_conn_info_t* info;
         BT_LOGW("%s, le conn null", __func__);
 
-        context.conn = bt_conn_lookup_addr_br((bt_addr_t*)addr);
+        info = bt_conn_find(addr, BT_TRANSPORT_BREDR);
+        context.conn = info->conn;
         if (!context.conn) {
             BT_LOGE("%s, br conn null", __func__);
             return BT_STATUS_FAIL;
