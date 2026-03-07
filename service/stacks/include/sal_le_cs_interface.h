1dfea5724b11a52e7e7a976edd347547c3c82d0c
Lu Jia <jialu@xiaomi.com>
2026-03-07 17:23:19 +0800

    cs: Move the HCI command into the worker.
    bug: v/87425
    
    Signed-off-by: jialu <jialu@xiaomi.com>

--- service/stacks/include/sal_le_cs_interface.h
+++ service/stacks/include/sal_le_cs_interface.h
@@ -117,8 +117,9 @@ bt_status_t bt_sal_cs_procedure_enable(bt_controller_id_t id, bt_address_t* addr
 bt_status_t bt_sal_cs_remove_config(bt_controller_id_t id, bt_address_t* addr, uint8_t config_id);
 bt_status_t bt_sal_cs_set_procedure_parameters(bt_controller_id_t id, bt_address_t* addr,
     const bt_le_srv_cs_set_procedure_parameters_param_t* params);
-bt_status_t bt_sal_cs_set_channel_classification(uint8_t channel_classification[10], bt_address_t* addr);
-bt_status_t bt_sal_cs_read_local_supported_capabilities(bt_srv_conn_le_cs_capabilities_t* params, bt_address_t* addr);
-bt_status_t bt_sal_cs_write_cached_remote_supported_capabilities(bt_srv_conn_le_cs_capabilities_t* params, bt_address_t* addr);
+bt_status_t bt_sal_cs_set_channel_classification(bt_controller_id_t id, uint8_t channel_classification[10], bt_address_t* addr);
+bt_status_t bt_sal_cs_read_local_supported_capabilities(bt_controller_id_t id, bt_srv_conn_le_cs_capabilities_t* params, bt_address_t* addr);
+bt_status_t bt_sal_cs_write_cached_remote_supported_capabilities(bt_controller_id_t id, const bt_srv_conn_le_cs_capabilities_t* params,
+    bt_address_t* addr);
 
 #endif //__SAL_LE_CS_INTERFACE_H__
