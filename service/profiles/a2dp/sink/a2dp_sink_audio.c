8861d83cf140421d2ddeb5b43e2f818feb3c196d
zhangyuan20 <zhangyuan20@xiaomi.com>
2026-03-10 13:53:48 +0800

    Tinycompress: send data only after received start
    Q95GTK-4624
    
    Rootcause: The `a2dp_sink_on_started` method is also called by `a2dp_state_machine`, causing it to run before the media sends the start command. Bluetooth premature data transmission prevents data from being cleared from the driver when media stops.
    
    Change-Id: I6f85cb6ad747003f66ba425faf719b760cb86a77
    Signed-off-by: zhangyuan20 <zhangyuan20@xiaomi.com>

--- service/profiles/a2dp/sink/a2dp_sink_audio.c
+++ service/profiles/a2dp/sink/a2dp_sink_audio.c
@@ -245,22 +245,10 @@ bool a2dp_sink_on_connection_changed(bool connected)
     return true;
 }
 
-void a2dp_sink_on_started(bool started)
-{
-    a2dp_sink_stream_t* stream = &sink_stream;
-
-    BT_LOGD("%s: %d", __func__, started);
-    audio_control_start(A2DP_SINK_PROFILE_ID, started);
-    if (started) {
-        if (stream->state != STATE_RUNNING) {
-            stream->state = STATE_RUNNING;
-        }
-    }
-}
-
 static void a2dp_sink_audio_handle_event(void* event)
 {
     a2dp_audio_event_t* audio_event = (a2dp_audio_event_t*)event;
+    a2dp_sink_stream_t* stream = &sink_stream;
 
     switch (audio_event->type) {
     case A2DP_AUDIO_EVENT_START:
@@ -269,7 +257,11 @@ static void a2dp_sink_audio_handle_event(void* event)
             break;
         }
 
-        a2dp_sink_on_started(true);
+        audio_control_start(A2DP_SINK_PROFILE_ID, true);
+        if (stream->state != STATE_RUNNING) {
+            stream->state = STATE_RUNNING;
+        }
+
         break;
     case A2DP_AUDIO_EVENT_STOP:
         a2dp_sink_stop_audio_req();
