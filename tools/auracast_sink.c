809f814238ea2eb751ee938a899bc34fa04775f8
Zihao Gao <gaozihao@xiaomi.com>
2026-03-10 19:27:13 +0800

    Auracast sink: allow to start extended scan
    bug: v/82380
    
    Add scan command to search active Auracast sources.
    
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- tools/auracast_sink.c
+++ tools/auracast_sink.c
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
+
+#include "bt_le_scan.h"
+#include "bt_pa_sync.h"
 #include "bt_tools.h"
 
 typedef struct {
@@ -36,18 +39,88 @@ static void usage(void)
     }
 }
 
+static void on_scan_result(bt_scanner_t* scanner, ble_scan_result_t* result)
+{
+    bt_status_t status;
+    bt_pa_sync_info_t* info;
+
+    if (g_auracast_sink->scanner != scanner)
+        return;
+
+    info = malloc(sizeof(bt_pa_sync_info_t));
+    if (info == NULL)
+        return;
+
+    status = bt_pa_sync_parse_adv_data(info, result);
+    if (status != BT_STATUS_SUCCESS)
+        return;
+}
+
+static void on_scan_status(bt_scanner_t* scanner, uint8_t status)
+{
+    PRINT("%s, status = %d", __func__, status);
+    if (g_auracast_sink->scanner != scanner)
+        return;
+
+    if (status != BT_STATUS_SUCCESS)
+        g_auracast_sink->scanner = NULL;
+}
+
+static void on_scan_stopped(bt_scanner_t* scanner)
+{
+    PRINT("%s", __func__);
+    if (g_auracast_sink->scanner != scanner)
+        return;
+
+    g_auracast_sink->scanner = NULL;
+}
+
+static const scanner_callbacks_t scanner_cbs = {
+    .size = sizeof(scanner_cbs),
+    .on_scan_result = on_scan_result,
+    .on_scan_start_status = on_scan_status,
+    .on_scan_stopped = on_scan_stopped,
+};
+
+static const ble_scan_settings_t default_scan_settings = {
+    .scan_mode = BT_SCAN_MODE_LOW_LATENCY,
+    .legacy = false,
+    .scan_type = BT_LE_SCAN_TYPE_PASSIVE,
+    .scan_phy = BT_LE_1M_PHY,
+    .policy.policy = 0, /**< Unfiltered */
+};
+
 int scan_cmd(void* handle, int argc, char* argv[])
 {
+    ble_scan_settings_t settings;
+
+    if (g_auracast_sink->scanner) {
+        PRINT("Already scanning");
+        return CMD_USAGE_FAULT;
+    }
+
+    memcpy(&settings, &default_scan_settings, sizeof(settings));
+
+    g_auracast_sink->scanner = bt_le_start_scan_settings(handle, &settings, &scanner_cbs);
+    if (g_auracast_sink->scanner == NULL)
+        return CMD_ERROR;
+
     return CMD_OK;
 }
 
 int auracast_sink_command_init(void* handle)
 {
+    g_auracast_sink = zalloc(sizeof(bttool_auracast_sink_t));
+    if (!g_auracast_sink)
+        return CMD_ERROR;
+
     return CMD_OK;
 }
 
 void auracast_sink_command_uninit(void* handle)
 {
+    free(g_auracast_sink);
+    g_auracast_sink = NULL;
 }
 
 int auracast_sink_command_exec(void* handle, int argc, char* argv[])
