f0852c1c3851ea1b40d88b1b3ccc7e042f4dd800
Zihao Gao <gaozihao@xiaomi.com>
2026-03-10 19:27:13 +0800

    PA sync: optimize logs at bttool.
    bug: v/82379
    
    Skip logs that are not available.
    Add more debug logs on error.
    
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- tools/utils.h
+++ tools/utils.h
@@ -14,10 +14,20 @@
  * limitations under the License.
  ***************************************************************************/
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "bluetooth.h"
+
+#define BTTOOL_STRCAT(dst, size, src, ...)                      \
+    do {                                                        \
+        size_t _len = strlen(dst);                              \
+        size_t _size = (size);                                  \
+        if (_len + 1 >= _size)                                  \
+            break;                                              \
+        snprintf(dst + _len, _size - _len, src, ##__VA_ARGS__); \
+    } while (0)
 
 bool phy_is_vaild(uint8_t phy);
 int le_addr_type(const char* str, ble_addr_type_t* type);
