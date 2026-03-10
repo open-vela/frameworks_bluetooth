87b6459b7fd765921172284c4b10f436603b4bf5
Zihao Gao <gaozihao@xiaomi.com>
2026-03-10 19:27:14 +0800

    PA sync: parse create sync command
    bug: v/82581
    
    This patch sends the create sync command to SAL.
    
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- service/src/pa_sync_service.c
+++ service/src/pa_sync_service.c
@@ -18,16 +18,85 @@
 #include "pa_sync_service.h"
 
 #include "bluetooth.h"
+#include "bt_list.h"
 #include "pa_sync_event.h"
+#include "sal_pa_sync_interface.h"
 #include "service_loop.h"
 
 #include "utils/log.h"
 
+typedef struct pa_sync_device {
+    bt_le_address_t addr;
+    uint8_t sid;
+    const bt_pa_sync_callbacks_t* cbs;
+    const void* context;
+} pa_sync_device_t;
+
 typedef struct pa_sync_info {
-    int nothing_but_wait_for_the_first_variable;
+    bt_list_t* sync_list;
 } pa_sync_info_t;
 
 static pa_sync_info_t* g_pa_sync_info = NULL;
+
+static void sync_terminated_callback(const bt_pa_sync_callbacks_t* cbs, const bt_le_address_t* addr,
+    uint8_t sid, const void* context)
+{
+    BT_LOGD("%s", __func__);
+
+    if (!cbs || !cbs->on_sync_terminated)
+        return;
+
+    cbs->on_sync_terminated(addr, sid, (void*)context);
+}
+
+static void create_sync(void* data)
+{
+    pa_sync_event_create_sync_t* msg = (pa_sync_event_create_sync_t*)data;
+    bt_sal_pa_sync_param_t params = { 0 };
+    pa_sync_device_t* device;
+
+    BT_LOGD("%s", __func__);
+
+    device = zalloc(sizeof(pa_sync_device_t));
+    if (device == NULL)
+        goto error;
+
+    memcpy(&params.addr, &msg->addr, sizeof(bt_le_address_t));
+    params.sid = msg->sid;
+    params.skip = msg->params.skip;
+    params.timeout = msg->params.timeout;
+    params.options &= ~BT_SAL_PA_SYNC_OPTION_USE_LIST; /**< periodic advertiser list unsupported */
+    params.options |= msg->params.no_report ? BT_SAL_PA_SYNC_OPTION_REPORTING_DISABLED : 0;
+    params.options |= msg->params.duplicate_filter ? BT_SAL_PA_SYNC_OPTION_FILTER_ENABLED : 0;
+    params.cte = 0; /**< nothing specified */
+    if (bt_sal_pa_create_sync(PRIMARY_ADAPTER, &params) != BT_STATUS_SUCCESS)
+        goto error;
+
+    /** sync created, add this device into list */
+    memcpy(&device->addr, &msg->addr, sizeof(bt_le_address_t));
+    device->sid = msg->sid;
+    device->cbs = msg->cbs;
+    device->context = msg->context;
+    bt_list_add_tail(g_pa_sync_info->sync_list, device);
+
+    free(msg);
+    return;
+
+error:
+    sync_terminated_callback(msg->cbs, &msg->addr, msg->sid, msg->context);
+    free(msg);
+}
+
+static void sync_removed(void* data)
+{
+    pa_sync_device_t* device = (pa_sync_device_t*)data;
+
+    BT_LOGD("%s", __func__);
+
+    sync_terminated_callback(device->cbs, &device->addr, device->sid, device->context);
+
+    free(device);
+}
 
 bt_status_t pa_sync_init(void)
 {
@@ -37,8 +106,19 @@ bt_status_t pa_sync_init(void)
         return BT_STATUS_BUSY;
 
     g_pa_sync_info = zalloc(sizeof(pa_sync_info_t));
+    if (!g_pa_sync_info)
+        return BT_STATUS_NOMEM;
+
+    g_pa_sync_info->sync_list = bt_list_new(sync_removed);
+    if (g_pa_sync_info->sync_list == NULL)
+        goto error;
 
     return BT_STATUS_SUCCESS;
+
+error:
+    bt_list_free(g_pa_sync_info->sync_list);
+    free(g_pa_sync_info);
+    return BT_STATUS_FAIL;
 }
 
 bt_status_t pa_sync_cleanup(void)
@@ -48,6 +128,7 @@ bt_status_t pa_sync_cleanup(void)
     if (!g_pa_sync_info)
         return BT_STATUS_DONE;
 
+    bt_list_free(g_pa_sync_info->sync_list);
     free(g_pa_sync_info);
     g_pa_sync_info = NULL;
 
