0f7d5193e99f09b779e8c94375612c9325375447
liyuheng <liyuheng@xiaomi.com>
2026-03-12 14:39:25 +0800

    hfp_hf: move callbacks lifecycle to init/cleanup
    bug: v/83395
    
    Rootcause: Upper-layer app does not unregister callbacks on BT off.
    
    Callbacks list was freed in hf_shutdown, losing registrations across BT on/off cycles. As a compatibility change, move callbacks to init/cleanup so registrations persist across BT enable/disable.
    
    Signed-off-by: liyuheng <liyuheng@xiaomi.com>

--- service/profiles/hfp_hf/hfp_hf_service.c
+++ service/profiles/hfp_hf/hfp_hf_service.c
@@ -197,7 +197,6 @@ static void hf_startup(profile_on_startup_t on_startup)
 
     service->max_connections = CONFIG_HFP_HF_MAX_CONNECTIONS;
     service->hf_devices = bt_list_new((bt_list_free_cb_t)hf_device_delete);
-    service->callbacks = bt_callbacks_list_new(CONFIG_BLUETOOTH_MAX_REGISTER_NUM);
     if (!service->hf_devices || !service->callbacks) {
         status = BT_STATUS_NOMEM;
         goto fail;
@@ -214,8 +213,6 @@ static void hf_startup(profile_on_startup_t on_startup)
 fail:
     bt_list_free(service->hf_devices);
     service->hf_devices = NULL;
-    bt_callbacks_list_free(service->callbacks);
-    service->callbacks = NULL;
     on_startup(PROFILE_HFP_HF, false);
 }
 
@@ -231,8 +228,6 @@ static void hf_shutdown(profile_on_shutdown_t on_shutdown)
     service->started = false;
     bt_list_free(service->hf_devices);
     service->hf_devices = NULL;
-    bt_callbacks_list_free(service->callbacks);
-    service->callbacks = NULL;
     bt_sal_hfp_hf_cleanup();
     on_shutdown(PROFILE_HFP_HF, true);
 }
@@ -375,9 +370,19 @@ static bt_status_t hfp_hf_init(void)
 {
     bt_status_t ret;
 
+    if (g_hfp_service.callbacks)
+        return BT_STATUS_SUCCESS;
+
+    g_hfp_service.callbacks = bt_callbacks_list_new(CONFIG_BLUETOOTH_MAX_REGISTER_NUM);
+    if (!g_hfp_service.callbacks) {
+        return BT_STATUS_NOMEM;
+    }
+
     ret = audio_ctrl_init();
     if (ret != BT_STATUS_SUCCESS) {
         BT_LOGE("%s: failed to start audio control channel", __func__);
+        bt_callbacks_list_free(g_hfp_service.callbacks);
+        g_hfp_service.callbacks = NULL;
         return ret;
     }
 
@@ -387,6 +392,8 @@ static bt_status_t hfp_hf_init(void)
 static void hfp_hf_cleanup(void)
 {
     audio_ctrl_cleanup();
+    bt_callbacks_list_free(g_hfp_service.callbacks);
+    g_hfp_service.callbacks = NULL;
 }
 
 static bt_status_t hfp_hf_startup(profile_on_startup_t cb)
@@ -439,7 +446,7 @@ static int hfp_hf_get_state(void)
 
 static void* hfp_hf_register_callbacks(void* remote, const hfp_hf_callbacks_t* callbacks)
 {
-    if (!g_hfp_service.started)
+    if (!g_hfp_service.callbacks)
         return NULL;
 
     return bt_remote_callbacks_register(g_hfp_service.callbacks, remote, (void*)callbacks);
@@ -447,7 +454,7 @@ static void* hfp_hf_register_callbacks(void* remote, const hfp_hf_callbacks_t* c
 
 static bool hfp_hf_unregister_callbacks(void** remote, void* cookie)
 {
-    if (!g_hfp_service.started)
+    if (!g_hfp_service.callbacks)
         return false;
 
     return bt_remote_callbacks_unregister(g_hfp_service.callbacks, remote, cookie);
