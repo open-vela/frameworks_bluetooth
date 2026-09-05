5bd58026c7aa08a5735083bee7263135e03324a7
bigcat2026 <bigcat_2026@163.com>
2026-09-05 21:36:19 +0800

    bluetooth: enable zblue HID implementation

--- service/stacks/zephyr/sal_gatt_client_interface.c
+++ service/stacks/zephyr/sal_gatt_client_interface.c
@@ -1381,7 +1381,11 @@ bt_status_t bt_sal_gatt_client_read_remote_rssi(bt_controller_id_t id, bt_addres
         return BT_STATUS_FAIL;
     }
 
+#ifdef BT_CONN_API_LE_READ_RSSI
     err = bt_conn_le_read_rssi(conn, &rssi);
+#else
+    err = bt_conn_read_rssi(conn, &rssi);
+#endif
     if (err) {
         BT_LOGE("%s, read rssi failed err:%d", __func__, err)
         return BT_STATUS_FAIL;
